ceaa12658bfc329daeedb1cb78369f3434ad32d0
Wild-Hound <wildhound404@gmail.com>
2022-08-22 00:10:37 +0600

    connect and retrive sample data from db

--- go.mod
+++ go.mod
@@ -5,6 +5,7 @@ go 1.18
 require (
 	github.com/joho/godotenv v1.4.0
 	github.com/labstack/echo/v4 v4.8.0
+	github.com/lib/pq v1.10.6
 )
 
 require (
--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"database/sql"
 	"fmt"
 	"net/http"
 	"os"
@@ -10,13 +11,60 @@ import (
 	"github.com/joho/godotenv"
 	"github.com/labstack/echo/v4"
 	"github.com/labstack/echo/v4/middleware"
+
+	_ "github.com/lib/pq"
 )
 
-func main(){
+type user struct{
+	id int
+	name string
+	score int
+}
+
+func init(){
+
 	err := godotenv.Load()
 	if(err != nil){
 		fmt.Println("error loading env")
 	}
+
+	connectionString := "postgresql://root:12345@127.0.0.1:5800/root?sslmode=disable"
+
+	conn, err := sql.Open("postgres",connectionString)
+	if(err != nil){
+		panic(err)
+	}
+	if err = conn.Ping(); err != nil{
+		panic(err)
+	}
+	defer conn.Close()
+
+	query := `SELECT * FROM "scors"`
+	rows, err := conn.Query(query)
+	if(err != nil){
+		fmt.Println("Error executing DB query")
+	}
+	defer rows.Close()
+
+	users := make([]user, 0)
+
+	for rows.Next(){
+		user := user{}
+		err := rows.Scan(&user.id, &user.name, &user.score)
+		if(err != nil){
+			panic(err)
+		}
+
+		users = append(users, user)
+		for _, ux := range(users){
+			fmt.Println(ux)
+		}
+	}
+	
+
+}
+
+func main(){
 	
 	e := echo.New()
 	e.Use(middleware.CORSWithConfig(middleware.CORSConfig{
